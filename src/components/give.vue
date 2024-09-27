3149256cd8a9dfe3813835e68e1ee0df380a0702
mtccoote <mtccoote@outlook.com>
2024-09-27 23:06:50 +0100

    add donation widget to give component

--- src/components/give.vue
+++ src/components/give.vue
@@ -1,26 +1,7 @@
 <template>
     <v-container class="size">
-        <v-row cols="2">
-            <v-col style="max-width: 35%" class="pa-6 mr-2">
-                <v-card style="background: #00aeef;">
-                    <v-col class="d-flex flex-column justify-center align-center">
-                        <v-tabs v-model="tab" fixed-tabs="true">
-                            <v-tab value="one-off">one-off</v-tab>
-                            <v-tab value="monthly">monthly</v-tab>
-                        </v-tabs>
-                        <v-card-text>
-                            <v-tabs-window v-model="tab">
-                                <v-tabs-window-item value="one-off">
-                                
-                                </v-tabs-window-item>
-                                <v-tabs-window-item value="one-off">
-                                Now you see me 
-                                </v-tabs-window-item>
-                            </v-tabs-window>
-                        </v-card-text>
-                    </v-col>
-                </v-card>
-            </v-col>
+        <v-row cols="1" justify="center">
+            <iframe src="https://donorbox.org/embed/give-184?default_interval=m&amount=20" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="yes" height="900px" width="100%" align-items="center" style="max-width: 423px; min-width: 250px; max-height:none!important" allow="payment"></iframe>
         </v-row>
     </v-container>
 </template>
